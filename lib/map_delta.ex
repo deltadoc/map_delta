a38c61a1219a3c8137832949c704b868e3a542e7
everzet <ever.zet@gmail.com>
2017-05-06 15:30:55 +0100

    Implement basic composition

--- lib/map_delta.ex
+++ lib/map_delta.ex
@@ -5,19 +5,99 @@ defmodule MapDelta do
 
   defstruct ops: []
 
-  alias MapDelta.Operation
+  alias MapDelta.{Operation, PropertyDelta}
 
   @type t :: %MapDelta{}
 
-  def add(prop, init), do: %MapDelta{ops: [Operation.add(prop, init)]}
+  def new, do: wrap([])
 
-  def remove(prop), do: %MapDelta{ops: [Operation.remove(prop)]}
+  def add(prop, init), do: wrap([Operation.add(prop, init)])
 
-  def replace(prop, init), do: %MapDelta{ops: [Operation.replace(prop, init)]}
+  def remove(prop), do: wrap([Operation.remove(prop)])
 
-  def change(prop, delta), do: %MapDelta{ops: [Operation.change(prop, delta)]}
+  def replace(prop, init), do: wrap([Operation.replace(prop, init)])
+
+  def change(prop, delta), do: wrap([Operation.change(prop, delta)])
+
+  def compose(%MapDelta{ops: [op_a]}, %MapDelta{ops: [op_b]}) do
+    op_a
+    |> do_compose(op_b)
+    |> List.wrap()
+    |> wrap()
+  end
+
+  defp do_compose(%{add: _}, %{add: _} = add_b) do
+    add_b
+  end
+
+  defp do_compose(%{add: _}, %{remove: _}) do
+    nil
+  end
+
+  defp do_compose(%{add: prop}, %{replace: _, init: init}) do
+    Operation.add(prop, init)
+  end
+
+  defp do_compose(%{add: prop, init: init}, %{change: prop, delta: delta}) do
+    Operation.add(prop, PropertyDelta.compose(init, delta))
+  end
+
+  defp do_compose(%{change: prop}, %{add: _, init: init}) do
+    Operation.replace(prop, init)
+  end
+
+  defp do_compose(%{change: _}, %{remove: _} = rem) do
+    rem
+  end
+
+  defp do_compose(%{change: _}, %{replace: _} = rep) do
+    rep
+  end
+
+  defp do_compose(%{change: prop, delta: delta_a},
+                  %{change: _, delta: delta_b}) do
+    Operation.change(prop, PropertyDelta.compose(delta_a, delta_b))
+  end
+
+  defp do_compose(%{remove: prop}, %{add: _, init: init}) do
+    Operation.replace(prop, init)
+  end
+
+  defp do_compose(%{remove: _}, %{remove: _} = rem_b) do
+    rem_b
+  end
+
+  defp do_compose(%{remove: _}, %{replace: _} = rep) do
+    rep
+  end
+
+  defp do_compose(%{remove: _} = rem, %{change: _}) do
+    rem
+  end
+
+  defp do_compose(%{replace: prop}, %{add: _, init: init}) do
+    Operation.replace(prop, init)
+  end
+
+  defp do_compose(%{replace: _}, %{remove: _} = rem) do
+    rem
+  end
+
+  defp do_compose(%{replace: _}, %{replace: _} = rep_b) do
+    rep_b
+  end
+
+  defp do_compose(%{replace: prop, init: init}, %{change: _, delta: delta}) do
+    Operation.replace(prop, PropertyDelta.compose(init, delta))
+  end
 
   def operations(%MapDelta{ops: ops}), do: ops
+
+  defp wrap(ops), do: %MapDelta{ops: ops}
+end
+
+defmodule MapDelta.PropertyDelta do
+  def compose(_first, second), do: second
 end
 
 defmodule MapDelta.Operation do
--- test/map_delta_test.exs
+++ test/map_delta_test.exs
@@ -22,5 +22,108 @@ defmodule MapDeltaTest do
     end
   end
 
+  describe "compose" do
+    test "add with add" do
+      a = MapDelta.add("a", nil)
+      b = MapDelta.add("a", 5)
+      assert MapDelta.compose(a, b) == b
+    end
+
+    test "add with remove" do
+      a = MapDelta.add("a", nil)
+      b = MapDelta.remove("a")
+      assert MapDelta.compose(a, b) == MapDelta.new()
+    end
+
+    test "add with replace" do
+      a = MapDelta.add("a", nil)
+      b = MapDelta.replace("a", 2)
+      assert MapDelta.compose(a, b) == MapDelta.add("a", 2)
+    end
+
+    test "add with change" do
+      a = MapDelta.add("a", 2)
+      b = MapDelta.change("a", 3)
+      assert MapDelta.compose(a, b) == MapDelta.add("a", 3)
+    end
+
+    test "change with add" do
+      a = MapDelta.change("a", 5)
+      b = MapDelta.add("a", 3)
+      assert MapDelta.compose(a, b) == MapDelta.replace("a", 3)
+    end
+
+    test "change with remove" do
+      a = MapDelta.change("a", 5)
+      b = MapDelta.remove("a")
+      assert MapDelta.compose(a, b) == b
+    end
+
+    test "change with replace" do
+      a = MapDelta.change("a", 3)
+      b = MapDelta.replace("a", nil)
+      assert MapDelta.compose(a, b) == b
+    end
+
+    test "change with change" do
+      a = MapDelta.change("a", 3)
+      b = MapDelta.change("a", 5)
+      assert MapDelta.compose(a, b) == MapDelta.change("a", 5)
+    end
+
+    test "remove with add" do
+      a = MapDelta.remove("a")
+      b = MapDelta.add("a", 3)
+      assert MapDelta.compose(a, b) == MapDelta.replace("a", 3)
+    end
+
+    test "remove with remove" do
+      a = MapDelta.remove("a")
+      b = MapDelta.remove("a")
+      assert MapDelta.compose(a, b) == b
+    end
+
+    test "remove with replace" do
+      a = MapDelta.remove("a")
+      b = MapDelta.replace("a", 5)
+      assert MapDelta.compose(a, b) == b
+    end
+
+    test "remove with change" do
+      # Technically this should never happen as
+      # it is obviously a broken-state operation,
+      # resulting in a change loss.
+      #
+      # But I'm covering it anyways, just in case.
+      a = MapDelta.remove("a")
+      b = MapDelta.change("a", 5)
+      assert MapDelta.compose(a, b) == a
+    end
+
+    test "replace with add" do
+      a = MapDelta.replace("a", 3)
+      b = MapDelta.add("a", 5)
+      assert MapDelta.compose(a, b) == MapDelta.replace("a", 5)
+    end
+
+    test "replace with remove" do
+      a = MapDelta.replace("a", 3)
+      b = MapDelta.remove("a")
+      assert MapDelta.compose(a, b) == b
+    end
+
+    test "replace with replace" do
+      a = MapDelta.replace("a", 3)
+      b = MapDelta.replace("a", 5)
+      assert MapDelta.compose(a, b) == b
+    end
+
+    test "replace with change" do
+      a = MapDelta.replace("a", 3)
+      b = MapDelta.change("a", 5)
+      assert MapDelta.compose(a, b) == MapDelta.replace("a", 5)
+    end
+  end
+
   defp ops(delta), do: MapDelta.operations(delta)
 end
