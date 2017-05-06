687b69304454fb08fb9e56c8f0b78026f85ad6a6
everzet <ever.zet@gmail.com>
2017-05-06 15:05:21 +0100

    Implement construction

--- lib/map_delta.ex
+++ lib/map_delta.ex
@@ -1,2 +1,45 @@
 defmodule MapDelta do
+  @moduledoc """
+  Delta format for maps.
+  """
+
+  defstruct ops: []
+
+  alias MapDelta.Operation
+
+  @type t :: %MapDelta{}
+
+  def add(prop, init), do: %MapDelta{ops: [Operation.add(prop, init)]}
+
+  def remove(prop), do: %MapDelta{ops: [Operation.remove(prop)]}
+
+  def replace(prop, init), do: %MapDelta{ops: [Operation.replace(prop, init)]}
+
+  def change(prop, delta), do: %MapDelta{ops: [Operation.change(prop, delta)]}
+
+  def operations(%MapDelta{ops: ops}), do: ops
+end
+
+defmodule MapDelta.Operation do
+  @moduledoc """
+  Map format for operations.
+  """
+
+  @type property :: String.t
+  @type delta :: any
+
+  @type add :: %{add: property, init: delta}
+  @type remove :: %{remove: property}
+  @type replace :: %{replace: property, init: delta}
+  @type change :: %{change: property, delta: delta}
+
+  @type t :: add | remove | replace | change
+
+  def add(prop, init), do: %{add: prop, init: init}
+
+  def remove(prop), do: %{remove: prop}
+
+  def replace(prop, init), do: %{replace: prop, init: init}
+
+  def change(prop, delta), do: %{change: prop, delta: delta}
 end
--- test/map_delta_test.exs
+++ test/map_delta_test.exs
@@ -1,4 +1,26 @@
 defmodule MapDeltaTest do
   use ExUnit.Case
   doctest MapDelta
+
+  alias MapDelta.Operation
+
+  describe "construct" do
+    test "add" do
+      assert ops(MapDelta.add("a", nil)) == [Operation.add("a", nil)]
+    end
+
+    test "remove" do
+      assert ops(MapDelta.remove("a")) == [Operation.remove("a")]
+    end
+
+    test "replace" do
+      assert ops(MapDelta.replace("a", 2)) == [Operation.replace("a", 2)]
+    end
+
+    test "change" do
+      assert ops(MapDelta.change("a", 5)) == [Operation.change("a", 5)]
+    end
+  end
+
+  defp ops(delta), do: MapDelta.operations(delta)
 end
