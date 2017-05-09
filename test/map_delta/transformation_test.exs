6e5da067d190e8814a577f4f3e673af68e7a6b99
everzet <ever.zet@gmail.com>
2017-05-10 00:12:01 +0100

    Refactor TransformationTest for better readability

--- test/map_delta/transformation_test.exs
+++ test/map_delta/transformation_test.exs
@@ -5,91 +5,29 @@ defmodule MapDelta.TransformationTest do
 
   import MapDelta.Generators
 
-  describe "transform" do
-    test "add / add" do
+  describe "transform add" do
+    test "against add" do
       a = MapDelta.add("a", nil)
       b = MapDelta.add("a", 5)
       assert MapDelta.transform(a, b, :left) == MapDelta.new()
       assert MapDelta.transform(b, a, :right) == MapDelta.replace("a", nil)
     end
 
-    test "add / remove" do
-      a = MapDelta.add("a", nil)
-      b = MapDelta.remove("a")
-      assert MapDelta.transform(a, b, :left) == MapDelta.new()
-      assert MapDelta.transform(b, a, :right) == a
-    end
-
-    test "add / replace" do
-      a = MapDelta.add("a", nil)
-      b = MapDelta.replace("a", 5)
-      assert MapDelta.transform(a, b, :left) == MapDelta.new()
-      assert MapDelta.transform(b, a, :right) == MapDelta.replace("a", nil)
-    end
-
-    test "add / change" do
-      a = MapDelta.add("a", nil)
-      b = MapDelta.change("a", 5)
-      assert MapDelta.transform(a, b, :left) == MapDelta.new()
-      assert MapDelta.transform(b, a, :right) == MapDelta.replace("a", nil)
-    end
-
-    test "remove / add" do
+    test "against remove" do
       a = MapDelta.remove("a")
       b = MapDelta.add("a", nil)
       assert MapDelta.transform(a, b, :left) == MapDelta.new()
       assert MapDelta.transform(b, a, :right) == a
     end
 
-    test "remove / remove" do
-      a = MapDelta.remove("a")
-      assert MapDelta.transform(a, a, :left) == MapDelta.new()
-      assert MapDelta.transform(a, a, :right) == MapDelta.new()
-    end
-
-    test "remove / replace" do
-      a = MapDelta.remove("a")
-      b = MapDelta.replace("a", nil)
-      assert MapDelta.transform(a, b, :left) == MapDelta.new()
-      assert MapDelta.transform(b, a, :right) == a
-    end
-
-    test "remove / change" do
-      a = MapDelta.remove("a")
-      b = MapDelta.change("a", 5)
-      assert MapDelta.transform(a, b, :left) == MapDelta.new()
-      assert MapDelta.transform(b, a, :right) == a
-    end
-
-    test "replace / add" do
+    test "against replace" do
       a = MapDelta.replace("a", 5)
       b = MapDelta.add("a", 3)
       assert MapDelta.transform(a, b, :left) == MapDelta.new()
       assert MapDelta.transform(b, a, :right) == a
     end
 
-    test "replace / remove" do
-      a = MapDelta.replace("a", 5)
-      b = MapDelta.remove("a")
-      assert MapDelta.transform(a, b, :left) == MapDelta.new()
-      assert MapDelta.transform(b, a, :right) == MapDelta.add("a", 5)
-    end
-
-    test "replace / replace" do
-      a = MapDelta.replace("a", 5)
-      b = MapDelta.replace("a", 2)
-      assert MapDelta.transform(a, b, :left) == MapDelta.new()
-      assert MapDelta.transform(b, a, :right) == a
-    end
-
-    test "replace / change" do
-      a = MapDelta.replace("a", 5)
-      b = MapDelta.change("a", 2)
-      assert MapDelta.transform(a, b, :left) == MapDelta.new()
-      assert MapDelta.transform(b, a, :right) == a
-    end
-
-    test "change / add" do
+    test "against change" do
       # Server doesn't win as the change is incremental,
       # but the add is destructive.
       a = MapDelta.change("a", 5)
@@ -97,8 +35,30 @@ defmodule MapDelta.TransformationTest do
       assert MapDelta.transform(a, b, :left) == MapDelta.replace("a", 2)
       assert MapDelta.transform(b, a, :right) == MapDelta.new()
     end
+  end
 
-    test "change / remove" do
+  describe "transform remove" do
+    test "against add" do
+      a = MapDelta.add("a", nil)
+      b = MapDelta.remove("a")
+      assert MapDelta.transform(a, b, :left) == MapDelta.new()
+      assert MapDelta.transform(b, a, :right) == a
+    end
+
+    test "against remove" do
+      a = MapDelta.remove("a")
+      assert MapDelta.transform(a, a, :left) == MapDelta.new()
+      assert MapDelta.transform(a, a, :right) == MapDelta.new()
+    end
+
+    test "against replace" do
+      a = MapDelta.replace("a", 5)
+      b = MapDelta.remove("a")
+      assert MapDelta.transform(a, b, :left) == MapDelta.new()
+      assert MapDelta.transform(b, a, :right) == MapDelta.add("a", 5)
+    end
+
+    test "against change" do
       # Server doesn't win as the change is incremental,
       # but the remove is destructive.
       a = MapDelta.change("a", 5)
@@ -106,8 +66,31 @@ defmodule MapDelta.TransformationTest do
       assert MapDelta.transform(a, b, :left) == MapDelta.remove("a")
       assert MapDelta.transform(b, a, :right) == MapDelta.new()
     end
+  end
 
-    test "change / replace" do
+  describe "transform replace" do
+    test "against add" do
+      a = MapDelta.add("a", nil)
+      b = MapDelta.replace("a", 5)
+      assert MapDelta.transform(a, b, :left) == MapDelta.new()
+      assert MapDelta.transform(b, a, :right) == MapDelta.replace("a", nil)
+    end
+
+    test "against remove" do
+      a = MapDelta.remove("a")
+      b = MapDelta.replace("a", nil)
+      assert MapDelta.transform(a, b, :left) == MapDelta.new()
+      assert MapDelta.transform(b, a, :right) == a
+    end
+
+    test "against replace" do
+      a = MapDelta.replace("a", 5)
+      b = MapDelta.replace("a", 2)
+      assert MapDelta.transform(a, b, :left) == MapDelta.new()
+      assert MapDelta.transform(b, a, :right) == a
+    end
+
+    test "against change" do
       # Server doesn't win as the change is incremental,
       # but the replace is destructive.
       a = MapDelta.change("a", 5)
@@ -115,8 +98,31 @@ defmodule MapDelta.TransformationTest do
       assert MapDelta.transform(a, b, :left) == b
       assert MapDelta.transform(b, a, :right) == MapDelta.new()
     end
+  end
 
-    test "change / change" do
+  describe "transform change" do
+    test "against add" do
+      a = MapDelta.add("a", nil)
+      b = MapDelta.change("a", 5)
+      assert MapDelta.transform(a, b, :left) == MapDelta.new()
+      assert MapDelta.transform(b, a, :right) == MapDelta.replace("a", nil)
+    end
+
+    test "against remove" do
+      a = MapDelta.remove("a")
+      b = MapDelta.change("a", 5)
+      assert MapDelta.transform(a, b, :left) == MapDelta.new()
+      assert MapDelta.transform(b, a, :right) == a
+    end
+
+    test "against replace" do
+      a = MapDelta.replace("a", 5)
+      b = MapDelta.change("a", 2)
+      assert MapDelta.transform(a, b, :left) == MapDelta.new()
+      assert MapDelta.transform(b, a, :right) == a
+    end
+
+    test "against change" do
       a = MapDelta.change("a", 3)
       b = MapDelta.change("a", 6)
       assert MapDelta.transform(a, b, :left) == a
