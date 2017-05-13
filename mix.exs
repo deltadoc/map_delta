98bbf4f6bed20164dc2b3c440cfa58900b62439f
everzet <ever.zet@gmail.com>
2017-05-13 14:32:30 +0100

    Remove dialyxir
    It is sadly more trouble than its worth at the moment.
    I will keep annotating methods, but I wouldn't use Dyalixir for now.

--- mix.exs
+++ mix.exs
@@ -8,23 +8,17 @@ defmodule MapDelta.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
-     aliases: aliases(),
-     dialyzer: [flags: ~w(-Werror_handling
-                          -Wrace_conditions
-                          -Wunderspecs
-                          -Wunmatched_returns),
-                ignore_warnings: ".dialyzer.ignore-warnings"]]
+     aliases: aliases()]
   end
 
   def application, do: []
 
   defp aliases do
-    [lint: ["credo --strict", "dialyzer --halt-exit-status"]]
+    [lint: ["credo --strict"]]
   end
 
   defp deps do
     [{:ex_doc, "~> 0.15", only: [:dev], runtime: false},
-     {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
      {:credo, "~> 0.7", only: [:dev, :test], runtime: false},
      {:eqc_ex, "~> 1.4", only: [:dev, :test], runtime: false}]
   end
